b6527312d7461cf8dc0ed763259f19dbf1c86957
Nedved SIMAO <109746899+NedSimao@users.noreply.github.com>
2023-06-07 23:20:18 +0200

    [WIP] Definition and implementation of the sketches

--- examples/TestBufferAsciiTime/TestBufferAsciiTime.ino
+++ examples/TestBufferAsciiTime/TestBufferAsciiTime.ino
@@ -1,7 +1,33 @@
-void setup() {
+#include <StreamChannel.hpp>
 
+#define SIZE 3
+uint16_t DATA[SIZE]={200, 128,150};
+
+unsigned long previousTime = 0;
+
+StreamBuffer Channel;
+
+void setup() {
+// put your setup code here, to run once:
+Serial.begin(9600);
+Channel.setMaxChannelNumber((uint8_t) SIZE);
 }
 
 void loop() {
+  /* Updates frequently */
+  previousTime = millis();
+  
+  Channel.appendChannelArray(DATA,SIZE);
+  
+  unsigned long currentTime = millis();
+
+  Channel.sendBufferAsciiTimeStamp(Serial, (currentTime - previousTime),"\t");
+
+   /* Update the timing for the next time around */
+   previousTime = currentTime;
+  
+  
 
 }
+
+
--- examples/TestBufferBinary16bitsTime/TestBufferBinary16bitsTime.ino
+++ examples/TestBufferBinary16bitsTime/TestBufferBinary16bitsTime.ino
@@ -1,7 +1,30 @@
-void setup() {
+#include <StreamChannel.hpp>
 
+#define SIZE 3
+uint16_t DATA[SIZE]={200, 128,150};
+
+
+StreamBuffer Channel;
+
+void setup() {
+Serial.begin(9600);
+Channel.setMaxChannelNumber((uint8_t) SIZE);
 }
 
 void loop() {
+  Channel.appendChannelArray(DATA,SIZE);
 
+  /* Updates frequently */
+  previousTime = millis();
+  
+  Channel.appendChannelArray(DATA,SIZE);
+  
+  unsigned long currentTime = millis();
+
+  Channel.sendBufferBinaryTimeStamp(Serial, (currentTime - previousTime));
+
+
+   /* Update the timing for the next time around */
+   previousTime = currentTime;
+  
 }
